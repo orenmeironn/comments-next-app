809d14f1ba9b5c1ff64e93940a2f1bf38f27a852
shahafc <77931661+shahafc@users.noreply.github.com>
2024-05-05 11:56:23 +0300

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,16 +1,32 @@
 import { delay } from "@/app/utils";
-import { NextResponse } from "next/server";
-
+import { NextRequest, NextResponse } from "next/server";
 const users = [
-  "https://robohash.org/24a6cda547939ea8a11aeec1d8e9e457?set=set4&bgset=&size=200x200",
-  "https://robohash.org/b8934a40b2298686320ca4055acf2bf6?set=set4&bgset=&size=200x200",
-  "https://robohash.org/caa7b569fd1e311aa906108736ea366e?set=set4&bgset=&size=200x200",
-  "https://robohash.org/d178f05c0002c0e668d06252a252efcb?set=set4&bgset=&size=200x200",
+  {
+    id: "1",
+    avatar:
+      "https://robohash.org/24a6cda547939ea8a11aeec1d8e9e457?set=set4&bgset=&size=200x200",
+  },
+  {
+    id: "2",
+    avatar:
+      "https://robohash.org/b8934a40b2298686320ca4055acf2bf6?set=set4&bgset=&size=200x200",
+  },
+  {
+    id: "3",
+    avatar:
+      "https://robohash.org/caa7b569fd1e311aa906108736ea366e?set=set4&bgset=&size=200x200",
+  },
+  {
+    id: "4",
+    avatar:
+      "https://robohash.org/d178f05c0002c0e668d06252a252efcb?set=set4&bgset=&size=200x200",
+  },
 ];
-
-export async function GET() {
-  await delay(1000);
+export const GET = async (request: NextRequest) => {
+  const userId = request.nextUrl.searchParams.get("id");
+  await delay(3000);
+  const user = users.find((user) => user.id == userId);
   return NextResponse.json({
-    url: users[Math.floor(Math.random() * users.length)],
+    url: user?.avatar,
   });
-}
+};
